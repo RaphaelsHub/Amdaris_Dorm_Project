7983f82fcb5dfb87fcdbce3c7854aeda32cd216b
RaphaelsHub <spekel2003@gmail.com>
2024-09-10 17:05:10 +0300

    Managed modelState Verification
    1. Created AuthController
    2. Login Request Model
    3. Registration Model

---
+++ Dorm/Dorm.Server/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using Dorm.Server.RequestData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using LoginRequest = Dorm.Server.RequestData.LoginRequest;
+using IdentityLoginRequest = Microsoft.AspNetCore.Identity.Data.LoginRequest;
+
+
+namespace Dorm.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+        private void ValidateRequest(IValidatableObject request)
+        {
+            var validationContext = new ValidationContext(request);
+            var validationResults = request.Validate(validationContext);
+
+            foreach (var validationResult in validationResults)
+            {
+                var memberName = validationResult.MemberNames.FirstOrDefault() ?? string.Empty;
+                var errorMessage = validationResult.ErrorMessage ?? string.Empty;
+                ModelState.AddModelError(memberName, errorMessage);
+            }
+        }
+
+        [HttpPost(Name = "login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            ValidateRequest(loginRequest);
+
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //Запросы определенные
+
+            return Ok();
+        }
+
+        [HttpPost(Name = "registration")]
+        public async Task<IActionResult> Registration([FromBody] RegistrationRequest registrationRequest)
+        {
+            ValidateRequest(registrationRequest);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //Какие-то там обращения к базе
+
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Dorm/Dorm.Server/RequestData/LoginRequest.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dorm.Server.RequestData
+{
+    public class LoginRequest : IValidatableObject
+    {
+        [Required(ErrorMessage = "Email field is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password field is required.")]
+        public string Password { get; set; } = string.Empty;
+
+        //for 2fa
+        [Required(ErrorMessage = "IpAddress field is required.")]
+        public string IpAddress { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Country field is required.")]
+        public string Country { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City field is required.")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "DeviceType field is required.")]
+        public string DeviceType { get; set; } = string.Empty;
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            //Check for lenght parament and expression value
+            if (Password.Length < 8 || !System.Text.RegularExpressions.Regex.IsMatch(Password, @"^[A-Za-z\d@$!%*?&]+$"))
+            {
+                results.Add(new ValidationResult("Password must contain at least 6 English letters(1 uppercase letter), 1 digit, and 1 special characters.",
+                    new[] { nameof(Password) }));
+            }
+
+            //Check for uppercase letters
+            if (!System.Text.RegularExpressions.Regex.IsMatch(Password, @"[A-Z]"))
+            {
+                results.Add(new ValidationResult("Password must contain at least 1 uppercase letter.", new[] { nameof(Password) }));
+            }
+
+            //Check for digits
+            if (!System.Text.RegularExpressions.Regex.IsMatch(Password, @"\d"))
+            {
+                results.Add(new ValidationResult("Password must contain at least 1 digit.", new[] { nameof(Password) }));
+            }
+
+            //Check for having special chacters
+            if (!System.Text.RegularExpressions.Regex.IsMatch(Password, @"[@$!%*?&]"))
+            {
+                results.Add(new ValidationResult("Password must contain at least 1 special character.", new[] { nameof(Password) }));
+            }
+
+            return new List<ValidationResult>();
+        }
+    }
+}
