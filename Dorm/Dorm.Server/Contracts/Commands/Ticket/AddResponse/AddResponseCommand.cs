023f4010b152c03f4779e39d90ac53dfc6cbfa20
Alexandr <alexandrvjosan@gmail.com>
2024-09-16 11:42:48 +0300

    Add AddResponse method and canEdit validation in GetAll method

---
+++ Dorm/Dorm.Server/Contracts/Commands/Ticket/AddResponse/AddResponseCommand.cs
@@ -0,0 +1,8 @@
+﻿using Dorm.Domain.DTO;
+using Dorm.Domain.Responces;
+using MediatR;
+
+namespace Dorm.Server.Contracts.Commands.Ticket.AddResponse
+{
+    public record AddResponseCommand (int ticketId, TicketDto ticketDto, string token) : IRequest<BaseResponse<TicketDto>>;
+}
--- Dorm/Dorm.Server/Program.cs
+++ Dorm/Dorm.Server/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddAuth(builder.Configuration);
 builder.Services.AddScoped<JwtService, JwtService>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5432;Database=DormHub;Username=postgres;Password=04nykk"));//04nykk
+    options.UseNpgsql("Host=localhost;Port=5432;Database=DormHub;Username=postgres;Password=root"));//04nykk
 
 
 
