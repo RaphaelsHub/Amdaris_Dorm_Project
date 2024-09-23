5a00a9d0406c085c7d5dff43a6b7d7bc7d28a106
IvanovaArina <147144869+IvanovaArina@users.noreply.github.com>
2024-09-23 12:28:56 +0300

    ticket form without css, autofilling user fields

--- Dorm/dorm.client/src/App.jsx
+++ Dorm/dorm.client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/login/LoginForm";
 import RegistrationForm from "./components/registration/RegistrationForm";
 import AdForm from "./components/baraholka/adForm/AdForm";
+import TicketForm from "./components/ticket/ticketForm/ticketForm";
 import AdPage from "./components/baraholka/adPage/AdPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/adform" element={<AdForm />} />
+        <Route path="/ticket" element={<TicketForm userId={2}/>} />//временно, пока нет аккаунта и не придумала как прокидывать айдишник пользователя между страницами
       </Routes>
     </Router>
   );
